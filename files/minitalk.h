1711bf253345663fe5033c075348b4e9092f43ec
Angela Lima <angelamcostalima@icloud.com>
2022-11-29 18:26:50 +0000

    :construction: wip -> server.c && client.c

--- files/minitalk.h
+++ files/minitalk.h
@@ -6,15 +6,16 @@
 /*   By: anlima <anlima@student.42lisboa.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/23 11:10:18 by anlima            #+#    #+#             */
-/*   Updated: 2022/11/25 13:02:00 by anlima           ###   ########.fr       */
+/*   Updated: 2022/11/29 18:26:17 by anlima           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINITALK_H
 # define MINITALK_H
 # include <unistd.h>
+# include <signal.h>
 
-void	ft_putnbr(long nbr);
+void	intrft_putnbr(long nbr);
 void	ft_putstr(char *str);
 
 #endif
--- files/server.c
+++ files/server.c
@@ -6,18 +6,30 @@
 /*   By: anlima <anlima@student.42lisboa.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/23 11:02:54 by anlima            #+#    #+#             */
-/*   Updated: 2022/11/25 13:03:04 by anlima           ###   ########.fr       */
+/*   Updated: 2022/11/29 16:39:39 by anlima           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
+void	sig_handle(int signal)
+{
+	ft_putstr("The handler was called\n");
+}
+
 int	main(void)
 {
-	int	pid;
+	int			pid;
 
 	pid = getpid();
+	signal(SIGINT, sig_handle);
 	ft_putstr("The server is up and running. It's PID is: ");
 	ft_putnbr(pid);
+	ft_putstr("\n");
+	while (1)
+	{
+		ft_putstr("Inside the main function\n");
+		sleep(1);
+	}
 	return (0);
 }
